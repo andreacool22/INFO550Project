12c2848a353e7183a189eb9cf01317a02e5db553
Andrea Cool <acool@emory.edu>
2020-10-22 14:55:45 -0400

    added necessary project components, created makefile, updated readme

---
+++ packages.r
@@ -0,0 +1,7 @@
+#! /usr/bin/env Rscript
+
+# install packages
+install.packages("dplyr", repos='http://cran.us.r-project.org') 
+install.packages("tidyverse", repos='http://cran.us.r-project.org')
+install.packages("lubridate", repos='http://cran.us.r-project.org')
+install.packages("table1", repos='http://cran.us.r-project.org')
